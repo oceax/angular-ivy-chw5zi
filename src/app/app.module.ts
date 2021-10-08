4a8fd96f09030bb4cafbf9c2329f34aa7d2cfa1e
oceax <orlandocea@gmail.com>
2021-10-07 22:51:17 -0600

    6 files modified, 9 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeaderComponent, FormComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
